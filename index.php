4a8f59cc71c83760ddc02da3f87c3ea77b95facf
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-06-16 07:55:43 +0700

    Update

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
   include('koneksi.php');
   if(isset($_SESSION['login_user'])){
   header("location: about.php");
@@ -7,55 +8,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Sistem Pakar</title>
+  <title>Diagnosis Smartphone</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
   <link rel="stylesheet" href="assets/css/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="icon" href="img/2.png">
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<link rel="icon" href="img/logo.png">
 </head>
 <body>
 
-<div class="menu">
-    <a href="index.php">
-      <h3> <font color="#4E931C">Exsyt </font>Phone</h3>
+<nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    <img src="img/logo.png" alt="" width="34" height="32" class="d-inline-block align-text-top">
+      Diagnosis Smartphone
     </a>
-<!--   untuk menu pada bagian header -->
-  <ul>
-      <p>
-        <button type="button" class="btn btn-primary   btn-block" id="myBtn">LOGIN</button>
-      </p>
-  </ul>
-<ul>
-  <p><a href="kerusakan.php"><button type="button" class="btn btn-primary btn-block">DAFTAR KERUSAKAN</button></a></p>
-</ul>
-
-  <ul>
-   <p>
-    <a href="diagnosa.php">
-      <button type="button" class="btn btn-primary btn-block">DIAGNOSA KERUSAKAN</button>
-    </a>
-  </p>
- </ul>
-
-  <ul>
-    <p>
-      <a href="about.php">
-        <button type="button" class="btn btn-primary btn-block">ABOUT</button>
-      </a>
-    </p>
-  </ul>
-
-  <ul>
-   <p>
-    <a href="index.php">
-      <button type="button" class="btn btn-primary btn-block">HOME</button>
-    </a>
-  </p>
- </ul>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        <a class="nav-link" href="about.php">About</a>
+        <a class="nav-link" href="diagnosa.php">Diagnosa Kerusakan</a>
+        <a class="nav-link" href="kerusakan.php">Daftar Kerusakan</a>
+        <a class="nav-link" href="#">Login</a>
+      </div>
+    </div>
   </div>
+</nav>
+
 
 <!--   Akhir bagian Menu pada Header -->
 
@@ -126,6 +111,7 @@ $(document).ready(function(){
     });
 });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <footer class="container-fluid text-center">
   <p>Copyright 2018 &copy;</p>
 </footer>
