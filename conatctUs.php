1f868d9cde0fa7316f5e5820e1ff17cadafc44fe
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-06-16 16:36:28 +0700

    update

--- conatctUs.php
+++ conatctUs.php
@@ -1,27 +1,17 @@
-<?php
-
-  include('koneksi.php');
-  if(isset($_SESSION['login_user'])){
-  header("location: about.php");
-}
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Diagnosis Smartphone</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<head>
+  <title>Sistem Pakar</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
+
+  <link rel="stylesheet" href="assets/css/styleAboutUs.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
-  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
 <link rel="icon" href="img/logo.png">
 </head>
 <body>
@@ -37,8 +27,8 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        <a class="nav-link" href="conatctUs.php">Contact Us</a>
+        <a class="nav-link"  href="index.php">Home</a>
+        <a class="nav-link active" aria-current="page" href="conatctUs.php">Contact Us</a>
         <a class="nav-link" href="diagnosa.php">Diagnosa Kerusakan</a>
         <a class="nav-link" href="kerusakan.php">Daftar Kerusakan</a>
         <a class="nav-link" href="login.php">Login</a>
@@ -48,14 +38,35 @@
 </nav>
 
 
-<main class="main-home">
-<div class="ui sizer vertical segment">
-  <div class="ui huge header">Selamat Datang di Diagnosis Smartphone</div>
-  <p>Diagnosisi Smartphone merupakan Sistem Pakar kerusakan pada smartphone Pada Android.Memudahkan orang awam dalam mengetahui kerusakan-kerusakan pada smartphone android.Serta Memberikan solusi kepada user mengenai tindakan apa yang harus dilakukan</p>
-</div>
-<img src="img/home pict.png">
-</main>
+<!--   Akhir bagian Menu pada Header -->
+<main>
+  <div>
+    <h1>Contact Us</h1>
+    <p>Hubungi kami dan beri tahu saya bagaimana kami dapat membantu</p>
+  </div>
 
+  <div class="toast-container position-static show">
+  <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-body">
+    <button class="ui instagram button">
+      <i class="instagram icon"></i>
+        ariefadji
+      </button>
+      <button class="ui twitter button">
+      <i class="twitter icon"></i>
+      ariefAdjie
+    </button>
+    <button class="ui facebook button">
+  <i class="facebook icon"></i>
+  Arief Adjie Nugroho
+</button>
+    </div>
+  </div>
+
+
+</div>
+
+</main>
 
 
 <!-- Button trigger modal -->
@@ -91,7 +102,6 @@
   </div>
 </div>
 
-
 <script>
 $(document).ready(function(){
     $("#myBtn").click(function(){
@@ -102,6 +112,8 @@ $(document).ready(function(){
 <footer class="container-fluid text-center">
   <p>Diagnosis Smartphone, Copyright 2022 &copy;</p>
 </footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
 </body>
 </html>
