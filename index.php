4907ac1e38db7c10d0135f0949adc479838165db
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-06-16 12:47:42 +0700

    Update

--- index.php
+++ index.php
@@ -11,11 +11,13 @@
   <title>Diagnosis Smartphone</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
   <link rel="stylesheet" href="assets/css/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
 <link rel="icon" href="img/logo.png">
 </head>
 <body>
@@ -23,7 +25,7 @@
 <nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-    <img src="img/logo.png" alt="" width="34" height="32" class="d-inline-block align-text-top">
+    <img src="img/logo-navbar.png" alt="" width="28" height="30" class="d-inline-block align-text-top">
       Diagnosis Smartphone
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,55 +37,38 @@
         <a class="nav-link" href="about.php">About</a>
         <a class="nav-link" href="diagnosa.php">Diagnosa Kerusakan</a>
         <a class="nav-link" href="kerusakan.php">Daftar Kerusakan</a>
-        <a class="nav-link" href="#">Login</a>
+        <a class="nav-link" href="#" onclick="document.getElementById('modalLogin').click()" >Login</a>
       </div>
     </div>
   </div>
 </nav>
 
 
-<!--   Akhir bagian Menu pada Header -->
-
-
-<div class="galery">
-
-
-<div class="judul">
-  Selamat datang di <B>EXSYSTPHONE</b>
-(Diagnosa Kerusakan Smartphone Android
-berbasis Web)
- <img src="img/g1.png" height="410">
-
+<main class="main-home">
+<div class="ui sizer vertical segment">
+  <div class="ui huge header">Selamat Datang di Diagnosis Smartphone</div>
+  <p>Diagnosisi Smartphone merupakan Sistem Pakar kerusakan pada smartphone Pada Android.Memudahkan orang awam dalam mengetahui kerusakan-kerusakan pada smartphone android.Serta Memberikan solusi kepada user mengenai tindakan apa yang harus dilakukan</p>
 </div>
-
-<div class="isi">
-ExsystPhone merupakan Sistem Pakar kerusakan pada smartphone
-Pada Android.Memudahkan orang awam dalam mengetahui kerusakan-kerusakan pada smartphone android.Serta
-Memberikan solusi kepada user mengenai tindakan apa yang harus dilakukan
-<br>
-<br>
-<br>
-
-<a href="#" class="btn btn-info" role="button">READ MORE</a>
-
-</div>
+<img src="img/home pict.png">
+</main>
 
 
-</div>
 
+<!-- Button trigger modal -->
+<button hidden type="button" id="modalLogin" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  
+</button>
 
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header" style="padding:35px 50px;">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
-        </div>
-        <div class="modal-body" style="padding:40px 50px;">
-          <form role="form" method="post" action="ceklogin.php">
+<!-- Modal -->
+<div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form role="form" method="post" action="ceklogin.php">
             <div class="form-group" method="post">
               <label for="username"><span class="glyphicon glyphicon-user"></span> Username</label>
               <input type="text" class="form-control" name="username" id="password" placeholder="Enter username">
@@ -92,17 +77,17 @@ Memberikan solusi kepada user mengenai tindakan apa yang harus dilakukan
               <label for="password"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
               <input type="password" class="form-control" name="password" id="password" placeholder="Enter password">
             </div>
-              <button type="submit" id="submit" nama="submit" class="btn btn-primary btn-block" method="post"><span class="glyphicon glyphicon-off"></span> Login</button>
+              <button hidden type="submit" id="submit" nama="submit" class="btn btn-primary btn-block" method="post"><span class="glyphicon glyphicon-off"></span> Login</button>
           </form>
-
-        </div>
       </div>
-
-
-
-
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" onclick="document.getElementById('submit').click()">Save changes</button>
+      </div>
     </div>
   </div>
+</div>
+
 
 <script>
 $(document).ready(function(){
@@ -113,7 +98,7 @@ $(document).ready(function(){
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <footer class="container-fluid text-center">
-  <p>Copyright 2018 &copy;</p>
+  <p>Diagnosis Smartphone, Copyright 2022 &copy;</p>
 </footer>
 </body>
 </html>
