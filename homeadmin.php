0ccf94cd12889f6931b66870739fcd31308d9292
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-06-18 18:48:07 +0700

    Update

--- homeadmin.php
+++ homeadmin.php
@@ -8,15 +8,70 @@ include "session.php";
   <title>Sistem Pakar</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="icon" href="img/2.png">
+  <link rel="stylesheet" href="assets/css/styleHomeAdmin.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+
+
+<link rel="icon" href="img/logo.png">
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
+<main>
+  <nav class="panel is-link">
+    <div>
+      <p class="panel-heading">
+        Hello, <?php echo $login_session; ?>
+      </p>
+    
+      <a class="panel-block is-active">
+        <span class="panel-icon">
+          <i class="icon home large" aria-hidden="true"></i>
+        </span>
+        Beranda
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="icon bug large" aria-hidden="true"></i>
+        </span>
+        Kerusakan
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="stethoscope large icon" aria-hidden="true"></i>
+        </span>
+        Ciri Kerusakan
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="book icon large" aria-hidden="true"></i>
+        </span>
+        Basis Pengetahuan
+      </a>
+      <a class="panel-block">
+        <span class="panel-icon">
+          <i class="first aid large icon" aria-hidden="true"></i>
+        </span>
+        Cara Mengatasi
+      </a>
+    </div>
+  
+    <div class="panel-block log-out">
+      <button class="button is-link is-outlined is-fullwidth">
+        Log Out
+      </button>
+    </div>
+  </nav>
+
+  <div>
+
+  </div>
+</main>
+
+
+
+<!-- <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -50,10 +105,10 @@ include "session.php";
       <p>Selamat datang <?php echo $login_session; ?>.Silahkan pilih menu yang diinginkan</p>
     </div>
   </div>
-</div>
+</div> -->
 
 <footer class="container-fluid text-center">
-  <p>Copyright 2018 &copy;</p>
+  <p>Diagnosis Smartphone, Copyright 2022 &copy;</p>
 </footer>
 
 </body>
